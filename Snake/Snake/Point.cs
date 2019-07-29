79724e7f75469e7a2825ca7a030b0687adebe62a
Dmitriy Proskurin <mail@diman.pro>
2019-07-30 00:38:29 +0300

    "Змейка" в движении

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -56,6 +56,12 @@ namespace Snake
             Console.WriteLine(sym);
         }
 
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
+
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
